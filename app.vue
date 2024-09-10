c7ad8937cff53ad3a67191bfb49f8591154ab04c
Daniyalkhan265 <daniyal.khan@ki5.co.uk>
2024-09-10 17:13:10 +0500

    fix

--- app.vue
+++ app.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <div>{{ text }}</div>
+  <button @click.native="checking">click me</button>
 </template>
+
+<script lang="ts" setup>
+const text = ref("this is testing project for nuxt booster");
+const checking = () => {
+  nextTick(() => {
+    console.log("checkingdsf");
+    text.value = "button is clicked";
+  });
+};
+</script>
