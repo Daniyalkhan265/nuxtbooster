d2cee735598b27958955ca371cfdfcaa9306085e
Daniyalkhan265 <daniyal.khan@ki5.co.uk>
2024-09-10 17:20:22 +0500

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['nuxt-booster'],
-
+  runtimeConfig: {
+    public: {
+      disableInfoLayer: false
+    }
+  },
 })
