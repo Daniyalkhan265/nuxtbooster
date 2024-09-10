d3bd4b80af8dcf96210bf9cc5eaf4ef9786f982a
Daniyalkhan265 <daniyal.khan@ki5.co.uk>
2024-09-10 18:30:34 +0500

    fix:test

--- app.vue
+++ app.vue
@@ -4,6 +4,7 @@
 </template>
 
 <script lang="ts" setup>
+import { ref, nextTick } from 'vue';
 const text = ref("this is testing project for nuxt booster");
 const checking = () => {
   nextTick(() => {
