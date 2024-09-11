edc4cfb9a57e3942cdb466848ca83eae200621fa
Daniyalkhan265 <daniyal.khan@ki5.co.uk>
2024-09-11 12:50:20 +0500

    fix: boster

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,6 @@ function getBuilder() {
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['nuxt-booster'],
   runtimeConfig: {
     public: {
       disableInfoLayer: false
