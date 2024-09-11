ad73ac7cde9f2d94708b4134c507b9a570be5bd2
Daniyalkhan265 <daniyal.khan@ki5.co.uk>
2024-09-11 17:28:08 +0500

    fix: boster

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-function getBuilder() {
-  const builder =
-      process.env.npm_config_builder || process.env.BUILDER || undefined;
-  return builder === 'webpack' ? '@nuxt/webpack-builder' : undefined;
-}
+
 export default defineNuxtConfig({
-    buildModules: [
-        '@nuxt/typescript-build', // Enables TypeScript support in Nuxt
-    ],
-  modern: 'client',
-  compatibilityDate: '2024-04-03',
+
   devtools: { enabled: true },
   modules: [
-    ['nuxt-booster', {
-      // ... other nuxt-booster options
-      preload: false
-    }]
+    ['nuxt-booster']
   ],
   runtimeConfig: {
     public: {
@@ -27,12 +16,6 @@ export default defineNuxtConfig({
   imports: {
     autoImport: false
   },
-  builder: getBuilder(),
-  build: {
-    // ... other build options
-    moduleOptions: {
-      preloading: true
-    }
-  },
+
   nuxtBooster: true
 })
