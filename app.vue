7016143d82c19c4630713ac10c76ca6bf59fdd7d
Daniyalkhan265 <daniyal.khan@ki5.co.uk>
2024-09-11 12:38:22 +0500

    fix: boster

--- app.vue
+++ app.vue
@@ -1,19 +1,31 @@
 <template>
-  <div>{{ text }}</div>
-  <button @click="checking">click me</button>
-  <button @click="checking" @touchstart="checking">click me</button>
+  <div id="app">
+    <div v-if="jsLoaded">
+      <!-- Your regular app content goes here -->
+      <div>{{ text }}</div>
+      <button @click="checking">click me</button>
+      <a href="https://nuxt.com/modules" ref="link">test</a>
+    </div>
 
-  <span ref="https://nuxt.com/modules">test</span>
-  <a href="https://nuxt.com/modules" ref="link">test</a>
+    <!-- Fallback message -->
+    <div v-else id="js-failure" style="color: red;">
+      JavaScript/TypeScript failed to load. Please check your browser settings.
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup defer>
-import { ref, nextTick } from 'vue';
+import { ref, onMounted } from 'vue';
+
 const text = ref("this is testing project for nuxt booster");
+const jsLoaded = ref(false); // Initially set to false
+
+// Simulate JavaScript loading by changing the flag on mount
+onMounted(() => {
+  jsLoaded.value = true; // JavaScript has successfully loaded
+});
+
 const checking = () => {
-  nextTick(() => {
-    console.log("checkingdsf");
-    text.value = "button is clicked";
-  });
+  text.value = "button is clicked";
 };
 </script>
