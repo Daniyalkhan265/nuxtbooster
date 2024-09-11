7cdbb62e815ce4b2364816717176d893f213f979
Daniyalkhan265 <daniyal.khan@ki5.co.uk>
2024-09-11 12:22:55 +0500

    fix: boster

--- app.vue
+++ app.vue
@@ -1,16 +1,19 @@
 <template>
   <div>{{ text }}</div>
-  <button @click="() => { text = 'button is clicked'; console.log('checkingdsf'); }">click me</button>
+  <button @click="checking">click me</button>
+  <button @click="checking" @touchstart="checking">click me</button>
+
+  <span ref="https://nuxt.com/modules">test</span>
   <a href="https://nuxt.com/modules" ref="link">test</a>
 </template>
 
 <script lang="ts" setup defer>
 import { ref, nextTick } from 'vue';
 const text = ref("this is testing project for nuxt booster");
-// const checking = () => {
-//   nextTick(() => {
-//     console.log("checkingdsf");
-//     text.value = "button is clicked";
-//   });
-// };
+const checking = () => {
+  nextTick(() => {
+    console.log("checkingdsf");
+    text.value = "button is clicked";
+  });
+};
 </script>
