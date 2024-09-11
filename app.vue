4327b4ee70c2fbd281a4604e16e74d06b0f29bd8
Daniyalkhan265 <daniyal.khan@ki5.co.uk>
2024-09-11 12:11:44 +0500

    fix: boster

--- app.vue
+++ app.vue
@@ -1,10 +1,10 @@
 <template>
   <div>{{ text }}</div>
   <button @click="checking">click me</button>
-  <span ref="https://nuxt.com/modules"></span>
+  <span ref="https://nuxt.com/modules">test</span>
 </template>
 
-<script lang="ts" setup>
+<script lang="ts" setup >
 import { ref, nextTick } from 'vue';
 const text = ref("this is testing project for nuxt booster");
 const checking = () => {
