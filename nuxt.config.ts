ed061ab5e39441e7a927f1608fdea16eb4ace18b
Daniyalkhan265 <daniyal.khan@ki5.co.uk>
2024-09-11 13:28:24 +0500

    fix: boster

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,33 +1,55 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { readPackage } from 'read-pkg';
-
 function getBuilder() {
   const builder =
       process.env.npm_config_builder || process.env.BUILDER || undefined;
   return builder === 'webpack' ? '@nuxt/webpack-builder' : undefined;
 }
 export default defineNuxtConfig({
-  const { repository } = await readPackage();
-
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['nuxt-booster'],
   runtimeConfig: {
     public: {
-      githubRepoUrl: repository.url.replace(/^git\+(.*)\.git$/, '$1'),
       disableInfoLayer: false
     }
   },
-
   ssr: true,
 
   imports: {
-    autoImport: true
+    autoImport: false
   },
   builder: getBuilder(),
-  build:{
-    transpile: ['vuetify'],
-
+  build: {
+    // ... other build options
+    moduleOptions: {
+      preloading: false
+    }
   },
-  booster: false
+  booster: {
+    debug: false,
+    // targetFormats: ['jpg|jpeg|png|gif'],
+    densities: 'x1 x2',
+
+    optimizeSSR: {
+      cleanPreloads: true,
+      cleanPrefetches: true,
+      inlineStyles: true
+    },
+
+    detection: {
+      performance: true,
+      browserSupport: true,
+      battery: true
+    },
+
+    performanceMetrics: {
+      device: {
+        hardwareConcurrency: { min: 2, max: 48 },
+        deviceMemory: { min: 2 }
+      },
+      timing: {
+        fcp: 800,
+        dcl: 1200 // fallback if fcp is not available (safari)
+      }
+    },}
 })
